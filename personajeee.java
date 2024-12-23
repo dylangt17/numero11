4eaa016f610beffffab66f595f1c7e6da484aaa8
Dylan Gia <anthony.gia@tecnologicoliceocristiano.edu.ec>
2024-12-22 23:50:52 -0500

    Add files via upload

---
+++ personajeee.java
@@ -0,0 +1,25 @@
+package Logica;
+
+public class personajeee {
+	  int id;
+	    String nombre;
+	    int salud;
+	    String Edad;
+		int nivel;
+		int agilidad;
+		String Frases;
+		String tipodevestimenta;
+		int carisma;
+		int defensa;
+		int velocidad;
+		String inventarios;
+		String tipodecomida;
+		String tiposdeamigos;
+		int saludos;
+		int tiposdeciudades;
+		int colordepelo;
+		String altura;
+		String fechadenacimiento;
+		int vidas;
+
+}
